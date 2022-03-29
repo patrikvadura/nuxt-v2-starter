08dfedcf08fa6d1ca26c7aacc5d72d6fa66c072c
Patrik Vaďura <patrikvadura@icloud.com>
2022-03-29 16:21:40 +0200

    add: scss styles default

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,46 @@
 <template>
-  <Tutorial />
+  <div class="nuxtStarter">
+    <nuxt-link
+      class="nuxtStarterLogo"
+      to="/"
+      target="_blank"
+    >
+      <NuxtLogo />
+    </nuxt-link>
+
+    <div class="pt-5">
+      <h2>
+        Welcome to your Nuxt v2 Starter
+      </h2>
+
+      <p>
+        I recommend you take a look at the <a href="https://nuxtjs.org" target="_blank">Nuxt documentation</a>, whether you are new or have previous experience with the framework.
+      </p>
+    </div>
+  </div>
 </template>
 
 <script>
+import NuxtLogo from '~/components/NuxtLogo.vue'
+
 export default {
-  name: 'IndexPage'
+  components: {
+    NuxtLogo
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+.nuxtStarter {
+  padding: 3rem;
+
+  &Logo {
+    @include flex ($direction: null, $justify: center, $align: center);
+  }
+
+  h2,
+  a {
+    color: $primary;
+  }
+}
+</style>
